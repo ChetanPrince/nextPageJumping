553afc99863ca7a74ac53bff0fee17bbd36c14cb
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-20 22:28:56 +0200

    elements selected

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const imageContainer = document.getElementById("container");
+    const images = document.querySelectorAll(".images");
+    const buttonsContainer = document.getElementById("buttons");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+})
