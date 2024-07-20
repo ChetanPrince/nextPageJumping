9890030e84a0b256cbcad4392da6e9b732f654a6
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-20 22:33:13 +0200

    btn is created and a common function is assigned

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const buttonsContainer = document.getElementById("buttons");
     const prevBtn = document.getElementById("prevBtn");
     const nextBtn = document.getElementById("nextBtn");
+    
+    function createButton(page){
+        const btn = buttonContainer.createElement("button");
+        btn.textContent = page;
+        btn.classList.add(".btn-css");
+        btn.id = `btn-${page}`;
+        btn.addEventListener("click", ()={
+            currentPage = page;
+            showPage(currentPage);
+        });
+        return btn;
+
+    }
 })
