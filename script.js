f51cf23fd0f051cca3dabe829bc6325b843a114e
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-21 17:01:00 +0200

    updatePageButton bug identified

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const buttonsContainer = document.getElementById("buttons");
     const prevBtn = document.getElementById("prevBtn");
     const nextBtn = document.getElementById("nextBtn");
+    let currentPage = 1;
+    let imagesPerPage = 4;
+    let totalPages = Math.ceil(images.length / imagesPerPage );
+
     
     function createButton(page){
-        const btn = buttonContainer.createElement("button");
+        const btn = buttonsContainer.createElement("button");
         btn.textContent = page;
         btn.classList.add(".btn-css");
         btn.id = `btn-${page}`;
-        btn.addEventListener("click", ()={
+        btn.addEventListener("click", ()=>{
             currentPage = page;
             showPage(currentPage);
         });
         return btn;
-
     }
+    function updatePageButton(page){
+        const pageButtons = buttonsContainer.querySelectorAll('.btn-css[id^="btns-"]');
+        pageButtons.forEach(buttons=>buttons.remove());
+
+        let start = Math.max(1, currentPage-1);
+        let end = Math.min(totalPages, currentPage+1);
+
+
+    } 
+
 })
