7321448f29ddcff96940edefb8df1cf14a21a899
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-21 17:43:04 +0200

    need clarification

--- script.js
+++ script.js
@@ -10,30 +10,59 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     
     function createButton(page){
-        const btn = buttonsContainer.createElement("button");
-        btn.textContent = page;
-        btn.classList.add(".btn-css");
-        btn.id = `btn-${page}`;
-        btn.addEventListener("click", ()=>{
+        const button = buttonsContainer.createElement("button");
+        button.textContent = page;
+        button.className = "btn-css";
+        button.id = `btn-${page}`;
+        button.addEventListener("click", ()=>{
             currentPage = page;
             showPage(currentPage);
         });
-        return btn;
+        return button;
     }
-    function updatePageButton(page){
-        const pageButtons = buttonsContainer.querySelectorAll(".bts-css[id^='btn-']");
+    function updatePageButton(){
+        const pageButtons = buttonsContainer.querySelectorAll('.btn-css[id^="btns-"]');
         pageButtons.forEach(button=> button.remove());
-        let start = Math.max(1, currentPage-1);
-        let end = Math.min(totalPages, currentPage+1);
+        let startPage = Math.max(1, currentPage-1);
+        let endPage = Math.min(totalPages, currentPage+1);
         if(currentPage ===1){
-            endPage = Math.max(totalPages, currentPage+2);
+            endPage = Math.min(totalPages, currentPage+2);
         }else if(currentPage === totalPages){
-            startPage = Math.min(1, currentPage-2);        }
+            startPage = Math.max(1, currentPage-2);        }
 
         for(let i=startPage; i<=endPage; i++){
-            buttonsContainer.insertBefore()
+            buttonsContainer.insertBefore(createButton(i),nextBtn);
         }
     }
 
+function showPage(page){
+    const start = (page-1) * imagesPerPage;
+    const end = page * imagesPerPage;
+    images.forEach((image, index)=>{
+        image.classList.toggle("hdie", !(index >=start && index < end));
+    });
+    updatePageButton();
+    const pageButtons = buttonsContainer.querySelectorAll('.btn-css[id^="btns-"]');
+    pageButtons.forEach((button,index)=>{
+        button.classList.toggle("active", button.textContent == page);
+    });
+    prevBtn.disabled = page  ===1;
+    nextBtn.disabled = page  ===totalPages;
+
+}
+prevBtn.addEventListener("click", ()=>{
+    if(currentPage > 1){
+        currentPage--;
+        showPage(currentPage);
+    }
+});
+nextBtn.addEventListener("click", ()=>{
+    if(currentPage < totalPages){
+        currentPage++;
+        showPage(currentPage);
+    }
+});
+updatePageButton();
+showPage(currentPage);
 
 })
